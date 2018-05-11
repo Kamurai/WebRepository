8d7ebdcb9729140cbdd212a73bb7a2e6f313c3c7
r2ucdk <r2ucdk@WSTP-PC0578PW-L.fanniemae.com>
2018-05-11 15:08:07 -0400

    Solution testing for AngularJS site

--- htdocs/AngularJS/Layout - Copy.js
+++ htdocs/AngularJS/Layout - Copy.js
@@ -3,15 +3,15 @@ vApp.controller("LayoutController", ['$scope', '$sce', function ($scope, $sce)
 	$scope.Navigation = function(vLevel)
 	{
 		$scope.vResult = "<span class='navlink'>";
-			$scope.vResult += "<a href='./AboutUs.html'>About Us</a>";
+			$scope.vResult += "<a href='"+GetPath(vLevel)+"AboutUs.html'>About Us</a>";
 		$scope.vResult += "</span>";
 		$scope.vResult += "<br>";
 		$scope.vResult += "<span class='navlink'>";
-			$scope.vResult += "<a href='./Media.html'>Media</a>";
+			$scope.vResult += "<a href='"+GetPath(vLevel)+"Media.html'>Media</a>";
 		$scope.vResult += "</span>";
 		$scope.vResult += "<br>";
 		$scope.vResult += "<span class='navlink'>";
-			$scope.vResult += "<a href='./Minecraft.html'>Minecraft!</a>";
+			$scope.vResult += "<a href='"+GetPath(vLevel)+"Minecraft.html'>Minecraft!</a>";
 		$scope.vResult += "</span>";
 		$scope.vResult += "<br>";
 		return $sce.trustAsHtml($scope.vResult);
