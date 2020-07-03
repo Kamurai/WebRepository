658a16f33709c625774ce33bc929053886bb079f
Kamurai <16156165+Kamurai@users.noreply.github.com>
2020-07-03 11:23:35 -0400

    adding React

---
+++ htdocs/React/Layout.js
@@ -0,0 +1,107 @@
+function buildLayout(vPage, vLevel, vDivision){
+    buildBody(vPage, vLevel);
+}
+
+function fillLayout(vPage, vLevel, vDivision){
+    fillHead(vLevel, vDivision);
+    fillBody(vPage, vLevel);
+}
+
+function fillNavigation(vLevel){
+	$( "#idCenterRowLeft" ).append( "<a class=\"navlinkA\" href=\""+getPath(vLevel)+"AboutUs.html\">About Us</a><br/><br/>"); //move to Custom
+    $( "#idCenterRowLeft" ).append( "<a class=\"navlinkA\" href=\""+getPath(vLevel)+"Media.html\">Media</a><br/><br/>"); //move to Custom
+    $( "#idCenterRowLeft" ).append( "<a class=\"navlinkA\" href=\""+getPath(vLevel)+"Minecraft.html\">Minecraft!</a><br/><br/>"); //move to Custom
+}
+
+
+function buildBody(vPage, vLevel){
+    var vGeneralLayout = document.createElement("div");
+    vGeneralLayout.setAttribute("id", "idGeneralLayout");
+    $( "body" ).append( vGeneralLayout );
+    
+    var vHeaderRow = document.createElement("Header");
+    vHeaderRow.setAttribute("id", "idHeaderRow");
+    $( "#idGeneralLayout" ).append( vHeaderRow );
+    
+    var vHeaderRowCenter = document.createElement("div");
+    vHeaderRowCenter.setAttribute("id", "idHeaderRowCenter");
+    $( "#idHeaderRow" ).append( vHeaderRowCenter );
+    
+    //TitlePicture(vLevel);
+    
+    var vNavigationRow = document.createElement("nav");
+    vNavigationRow.setAttribute("id", "idNavigationRow");
+    $( "#idGeneralLayout" ).append( vNavigationRow );
+    
+    var vNavigationBar = document.createElement("div");
+    vNavigationBar.setAttribute("id", "idNavigationBar");
+    $( "#idNavigationRow" ).append( vNavigationBar );
+    
+    //NavBar(vLevel);
+    
+    var vCenterRow = document.createElement("div");
+    vCenterRow.setAttribute("id", "idCenterRow");
+    $( "#idGeneralLayout" ).append( vCenterRow );
+    
+    var vCenterRowLeft = document.createElement("div");
+    vCenterRowLeft.setAttribute("id", "idCenterRowLeft");
+    $( "#idCenterRow" ).append( vCenterRowLeft );
+    
+    //NavigationHeader(vCenterRowLeft);
+    
+    //Navigation(vLevel)
+    
+
+    var vCenterRowMain = document.createElement("div");
+    vCenterRowMain.setAttribute("id", "idCenterRowMain");
+    $( "#idCenterRow" ).append( vCenterRowMain );
+    
+    //Title(vPage);
+    //Header(vPage, vCenterRowMain);
+    //Content(vPage, vCenterRowMain);
+    
+    var vCenterRowRight = document.createElement("div");
+    vCenterRowRight.setAttribute("id", "idCenterRowRight");
+    $( "#idCenterRow" ).append( vCenterRowRight );
+
+    //InformationHeader(vCenterRowRight);
+    //Versions(vPage, vCenterRowRight);
+
+    var vFooterRow = document.createElement("Footer");
+    vFooterRow.setAttribute("id", "idFooterRow");
+    $( "#idGeneralLayout" ).append( vFooterRow );
+    
+    var vFooterRowMain = document.createElement("div");
+    vFooterRowMain.setAttribute("id", "idFooterRowMain");
+    $( "#idFooterRow" ).append( vFooterRowMain );
+    
+    var vFooterContent = document.createElement("div");
+    vFooterContent.setAttribute("id", "idFooterContent");
+    $( "#idFooterRowMain" ).append( vFooterContent );
+        
+    var vFooterManagement = document.createElement("div");
+    vFooterManagement.setAttribute("id", "idFooterManagement");
+    $( "#idFooterRowMain" ).append( vFooterManagement );
+        
+    //Footer();
+    //WebMaster();
+}
+
+function fillBody(vPage, vLevel){
+    fillLogo(vLevel);
+    fillNavBar(vLevel);
+    
+    fillNavigationHeader();
+    fillNavigation(vLevel)
+    
+    fillTitle(vPage);
+    fillHeader(vPage);
+    fillContent(vPage);
+    
+    fillInformationHeader();
+	fillInformation();
+    fillVersions(vPage);
+
+    fillFooter();
+    fillWebMaster();
+}
---
+++ htdocs/React/Universal.js
@@ -0,0 +1,245 @@
+function startHead(){
+	vResult = "";
+	
+	vResult += "<head>";
+	vResult += "</head>";
+	
+	return vResult;
+}
+
+function startBody(){
+	vResult = "";
+	
+	vResult += "<body>";
+	vResult += "</body>";
+	
+	return vResult;
+}
+
+function fillHead(vLevel, vDivision){
+	ReactDOM.render(
+		getHead(vLevel, vDivision),
+		document.getElementById('root')
+	);
+}
+
+function getHead(vLevel, vDivision){
+	const cHead = "";
+    
+	cHead += <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">;
+    cHead += getStyle(vLevel, vDivision);
+	
+	return cHead;
+}
+
+function getStyle(vLevel, vDivision){
+	var vResult = "";
+	
+	vResult += "<link href=\"http://htkb.dyndns.org/Styles/";
+	vResult += getStyleFile(vDivision) + "\" rel=\"stylesheet\" type=\"text/css\">";
+	
+	return vResult;
+}
+
+function getStyleFile(vDivision){
+    var vResult = "";
+    var vDefault = "Main.css";
+    
+    if( vDivision == 0 )
+    {
+        vResult += vDefault;
+    }
+    else if( vDivision == 1 )
+    {
+        vResult += "Division1.css";
+    }
+    else if( vDivision == 2 )
+    {
+        vResult += "Division2.css";
+    }
+    else if( vDivision == 3 )
+    {
+        vResult += "Division3.css";
+    }
+    else
+    {
+        vResult += vDefault;
+    }
+    
+    return vResult;
+}
+
+function fillLogo(vLevel)
+{
+    $( "#idHeaderRowCenter" ).append( getLogo(vLevel) );
+}
+
+function getLogo(vLevel)
+{
+	var vResult = "";
+	
+    vResult += "<img id=\"idLogo\" src='http://htkb.dyndns.org/Pictures/logoHTKB.jpg'><br>";
+	
+	return vResult;
+}
+
+function fillNavBar(vLevel)
+{
+    $( "#idNavigationBar" ).append( getNavBar(vLevel) );
+}
+
+function getNavBar(vLevel)
+{
+	var vResult = "";
+	
+    vResult += "<a class=\"navBar\" href='"+getPath(vLevel)+"Index.html'>Home</a>";
+    vResult += "<a class=\"navBar\" href='"+getPath(vLevel)+"Division1/Index.html'>Web Programming</a>";
+    vResult += "<a class=\"navBar\" href='"+getPath(vLevel)+"Division2/Index.html'>Private Projects</a>";
+    vResult += "<a class=\"navBar\" href='"+getPath(vLevel)+"Division3/Index.html'>Downloadable Projects</a>";
+	
+	return vResult;
+}
+
+function fillNavigationHeader()
+{
+    var tableRowCenterLeftHeader = document.createElement("h4");
+    tableRowCenterLeftHeader.textContent = getNavigationHeader();
+    $( "#idCenterRowLeft" ).append( tableRowCenterLeftHeader );
+}
+
+function getNavigationHeader()
+{
+    var vResult = "";
+	
+    vResult += "Navigation";
+	
+	return vResult;
+}
+
+function fillInformationHeader()
+{
+    var tableRowCenterRightHeader = document.createElement("h4");
+    tableRowCenterRightHeader.textContent = getInformationHeader();
+    $( "#idCenterRowRight" ).append( tableRowCenterRightHeader );
+}
+
+function getInformationHeader()
+{
+    var vResult = "";
+	
+    vResult += "Information";
+	
+	return vResult;
+}
+
+function fillInformation()
+{
+    $( "#idCenterRowRight" ).append( getInformation() );
+}
+
+function getInformation()
+{
+    var vResult = "";
+	
+    vResult += "This is written with basic HTML and javascript.<br><br>";
+	vResult += "Other versions of this page are here:<br>";
+	
+	return vResult;
+}
+
+function fillGDR()
+{
+    $( "#idFooterContent" ).append( getGDR );
+}
+
+function getGDR()
+{
+	var vResult = "";
+	
+    vResult += "<a href='http://htkb.dyndns.org/downloads/GDR.zip'>You can download my Games Development Report here.</a></br>";
+	
+	return vResult;
+}
+
+function fillWinRar()
+{
+    $( "#idFooterContent" ).append( getWinRar() );
+}
+
+function getWinRar()
+{
+	var vResult = "";
+	
+    vResult += "<a href='http://htkb.dyndns.org/downloads/wrar371.exe'>You may need WinRar to open zip files from this site.</a></br>";
+	
+	return vResult;
+}
+
+function fillFooter()
+{
+    $( "#idFooterContent" ).append( getFooter() );
+}
+
+function getFooter()
+{
+    var vResult = "";
+	
+    vResult += "© Copyright 2012 All rights reserved<br>";    
+    vResult += "House That Kamurai Built<br>";
+	
+	return vResult;   
+}
+
+function fillWebMaster()
+{
+    $( "#idFooterManagement" ).append( getWebMaster() );
+}
+
+function getWebMaster()
+{
+    var vResult = "";
+	
+    vResult += "Website managed by Kamurai.";
+	
+	return vResult;
+}
+
+function getPath(vLevel)
+{
+	var vResult = "";
+	
+    if(vLevel <= 0)
+    {
+        vResult = "./";
+    }
+    else if(vLevel == 1)
+    {
+        vResult = "../";
+    }
+    else if(vLevel == 2)
+    {
+        vResult = "../../";
+    }
+    else if(vLevel == 3)
+    {
+        vResult = "../../../";
+    }
+    else if(vLevel == 4)
+    {
+        vResult = "../../../../";
+    }
+    else if(vLevel == 5)
+    {
+        vResult = "../../../../../";
+    }
+    else if(vLevel == 6)
+    {
+        vResult = "../../../../../../";
+    }
+    else if(vLevel == 7)
+    {
+        vResult = "../../../../../../../";
+    }
+	
+	return vResult;
+}
